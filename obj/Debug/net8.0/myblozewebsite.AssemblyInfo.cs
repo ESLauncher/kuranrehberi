383856c0994bac96b96ab58050387db60fa5b1fa
ESLauncher <minecree5963@google.com>
2026-08-27 16:35:01 +0300

    1.0.1.2 google console hataları düzeltildi

--- obj/Debug/net8.0/myblozewebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/myblozewebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myblozewebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b9210aca17298f726c171572e806354696a4b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd79da0ca88d814ac3b2d9f048b4c58ec82d1ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("myblozewebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myblozewebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

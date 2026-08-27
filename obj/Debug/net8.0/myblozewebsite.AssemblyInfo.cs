f25a6f93233ed09d0ae83f0bf7816d7f2eb30695
ESLauncher <minecree5963@google.com>
2026-08-27 16:37:34 +0300

    first upgrade

--- obj/Debug/net8.0/myblozewebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/myblozewebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myblozewebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd79da0ca88d814ac3b2d9f048b4c58ec82d1ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+383856c0994bac96b96ab58050387db60fa5b1fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("myblozewebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myblozewebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
